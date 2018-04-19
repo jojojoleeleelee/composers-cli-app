a47cb924e853deb9d222cd8b206a20400e3159b9
Joanne Lee <vernapii@gmail.com>
2018-04-19 07:53:57 +0000

    cli commands

--- lib/composers/cli.rb
+++ lib/composers/cli.rb
@@ -1,15 +1,8 @@
 class Composers::CLI
-attr_accessor :input
 
   def call
     puts "Want to learn more about Composers?"
-    menu
-  end
-
-  def menu
-    puts "Type your favorite letter from the alphabet:"
-    input = gets.strip.downcase[0]
-    @input = input
+    start
   end
 
   def to_number
@@ -18,4 +11,28 @@ attr_accessor :input
     return number
   end
 
+  def alphabet_selection
+    chosen = input.to_number
+  end
+
+  def start
+    input = nil
+    while input != "exit" || input != "stop"
+      puts ""
+      puts "Type your favorite letter from the alphabet:"
+      puts "C'mon it can't be that hard..."
+      puts "You have exactly 27 choices!"
+      puts "Enter exit to end"
+      puts "Also, you're in for a fun surprise!"
+      input = gets.strip.downcase[0]
+      if input.is_a? String
+        Composers::ComposersFunction.list_of_composers(input.to_number)
+        puts "Now, choose the person you want to learn more about by typing the corresponding number of that composer."
+
+      else
+        start
+      end
+    end
+    puts "Buh-Bye! Remember to listen to some Bach :)"
+  end
 end
--- lib/composers/composers_function.rb
+++ lib/composers/composers_function.rb
@@ -2,19 +2,21 @@
 class Composers::ComposersFunction
   attr_accessor :name, :bio, :year, :works, :url
 
-  def alphabet_selection
-    # chosen = @input.to_number
+  def initialize(name = nil, url = nil)
+    @name = name
+    @url = url
   end
 
-  def self.list_of_composers
+
+  def self.list_of_composers(input)
     html = open("http://www.classical.net/music/composer/masterindex.php")
     scrape = Nokogiri::HTML(html)
     collection = Array.new
-    # i = 0
-    # while i < scrape.css('.worklist')["#{chosen}"].css('li').length
-    #   collection << scrape.css('.worklist').css('li').css('a')[i].children.text
-    #   i += 1
-    # end
+    i = 0
+    while i < scrape.css('.worklist')["#{input}"].css('li').length
+      collection << scrape.css('.worklist').css('li').css('a')[i].children.text
+      i += 1
+    end
 
     collection.each.with_index(1) do |name, idx|
       puts "#{idx}. - #{name}"
